dfd4d58999855b03fca232be04146f9298a686bc
reZach <reZach@users.noreply.github.com>
2024-08-18 23:47:13 -0500

    Adding more advent verses

--- CatholicDailyReadings.Business/BibleProvider.cs
+++ CatholicDailyReadings.Business/BibleProvider.cs
@@ -58,6 +58,24 @@ namespace CatholicDailyReadings.Business
 
         private DailyReading? GetAdventReading(DateTime advent, DateTime date, Year year)
         {
+            // Check feast days first
+
+            // Immaculate Conception
+            DateTime immaculateConception = new DateTime(date.Year, 12, 8);
+
+            // If the Immaculate Conception falls on a Sunday, the readings
+            // are said on the following day
+            if ((immaculateConception.DayOfWeek == DayOfWeek.Sunday && date.Month == 12 && date.Day == 9) ||
+                date.Month == 12 && date.Day == 8)
+                return new DailyReading { FirstReading = "Gn 3:9-15, 20", SecondReading = "Eph 1:3-6, 11-12", Gospel = "Lk 1:26-38" };
+
+            // Our Lady of Guadalupe
+            if (date.Month == 12 && date.Day == 12)
+                return new DailyReading { FirstReading = "Zec 2:14-17", Gospel = "Lk 1:39-47" };
+
+
+
+
             TimeSpan difference = date.Subtract(advent);
             int dayDifference = (int)difference.TotalDays;
 
@@ -91,6 +109,37 @@ namespace CatholicDailyReadings.Business
                     5 => new DailyReading { FirstReading = "Is 29:17-24", Gospel = "Mt 9:27-31" },
                     // Saturday
                     6 => new DailyReading { FirstReading = "Is 30:19-21, 23-26", Gospel = "Mt 9:35—10:1, 5a, 6-8" },
+                    _ => null
+                };
+            }
+            else if (dayDifference >= 7 && dayDifference < 14)
+            {
+                // Sunday
+                if (dayDifference == 7)
+                {
+                    return year switch
+                    {
+                        Year.A => new DailyReading { FirstReading = "Is 11:1-10", SecondReading = "Rom 15:4-9", Gospel = "Mt 3:1-12" },
+                        Year.B => new DailyReading { FirstReading = "Is 40:1-5, 9-11", SecondReading = "2 Pt 3:8-14", Gospel = "Mk 1:1-8" },
+                        Year.C => new DailyReading { FirstReading = "Bar 5:1-9", SecondReading = "Phil 1:4-6, 8-11", Gospel = "Lk 3:1-6" },
+                        _ => null
+                    };
+                }
+
+                return dayDifference switch
+                {
+                    // Monday
+                    8 => new DailyReading { FirstReading = "Is 35:1-10", Gospel = "Lk 5:17-26" },
+                    // Tuesday
+                    9 => new DailyReading { FirstReading = "Is 40:1-11", Gospel = "Mt 18:12-14" },
+                    // Wednesday
+                    10 => new DailyReading { FirstReading = "Is 40:25-31", Gospel = "Mt 11:28-30" },
+                    // Thursday
+                    11 => new DailyReading { FirstReading = "Is 41:13-20", Gospel = "Mt 11:11-15" },
+                    // Friday
+                    12 => new DailyReading { FirstReading = "Is 48:17-19", Gospel = "Mt 11:16-19" },
+                    // Saturday
+                    13 => new DailyReading { FirstReading = "Sir 48:1-4, 9-11", Gospel = "Mt 17:9a, 10-13" },
                     _ => null
                 };
             }
